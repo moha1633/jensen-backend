2152d4c574fb8057df04bf83418556d52b9edd96
moha1633 <mgudaal6@gmail.com>
2022-01-25 13:43:21 +0100

    prepared backend for heroku

--- app.js
+++ app.js
@@ -1,15 +1,57 @@
+const credentials = {secretUser:"user" , secretPassword:"password"}
+
+const cors = require("cors")
 const express = require("express")
- const app = express()
+const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken');
 
- process.env.PORT = 3000
- app.get("/", (req ,res)=>{
-    res.set('WWW-Authenticate','Basic realm="Access to index"')
-    res.status(200).send("Unauthorised access")
-    })
-    
+const app = express()
+const PORT = process.env.PORT || 4000
 
-    app.listen(3000 , ()=>{
-        console.log('STARTED LISTENING ON PORT ${process.env.PORT}')
+app.use(function (req, res, next) {
+   res.setHeader('Content-Security-Policy', "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'");
+   next();
+});
+
+app.use('/healthcheck', require('./routes/healthcheck.routes'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+
+app.get("/", (req ,res)=>{
+   headers={"http_status":200, "cache-control":  "no-cache"}
+   body={"status": "available"}
+   res.status(200).send(body)
+})
+
+app.get("/health", (req ,res)=>{
+   headers={"http_status":200, "cache-control":  "no-cache"}
+   body={"status": "available"}
+   res.status(200).send(body)
+})
 
 
+app.post('/authorize', (req, res) => {
+   // Insert Login Code Here
+   let user = req.body.user;
+   let password = req.body.password;
+   console.log(`User ${user}`)
+   console.log(`Password ${password}`)
+
+   if(user===credentials.secretUser && password===credentials.secretPassword){
+      console.log("Authorized")
+      const token = jwt.sign({
+            data: 'foobar'
+      }, 'your-secret-key-here', { expiresIn: 60 * 60 }); 
+
+      console.log(token)
+      res.status(200).send(token)
+  }else{
+      console.log("Not authorized")
+      res.status(200).send({"STATUS":"FAILURE"})
+   }
+});
+
+app.listen(PORT , ()=>{
+     console.log(`STARTED LISTENING ON PORT ${PORT}`)
 });
